896f87fab1e469bfe734e42f894cfd8aaaaefa02
haz323 <mr.yanonly@gmail.com>
2025-03-07 12:13:37 -0500

    update on google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+import { GoogleAnalytics } from '@next/third-parties/google';
+
 import '@/app/style/normalize.css';
 import '@/app/style/globals.css';
 
@@ -59,6 +61,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      {process.env.NODE_ENV === 'production' && <GoogleAnalytics gaId='G-F2VQ070RJY' />}
       <body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
     </html>
   );
