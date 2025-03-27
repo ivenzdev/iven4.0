6a6a46f422a12f29b3c9eadd7aad32ea81e4ea2c
haz323 <mr.yanonly@gmail.com>
2025-03-27 15:10:09 -0400

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,13 +8,13 @@ export const metadata: Metadata = {
   title: process.env.NEXT_PUBLIC_DEFAULT_LANGUAGE === 'zh' ? '张浩贤 ｜ 创始人' : 'Iven Zhang | Software Engineer',
   description:
     process.env.NEXT_PUBLIC_DEFAULT_LANGUAGE === 'zh'
-      ? '专修 React.js 和 Next.js，创业公司全能牛马。前后端通吃, UI 亲自画, 咖啡机坏了也修。我老板曾高度评价: Iven, 你一个人能干二十头牛马的活。我谢谢你。'
+      ? '专修 React.js 和 Next.js，创业公司全能牛马。前后端通吃, UI 亲自画, 咖啡机坏了也修。我老板曾高度评价我一个人能干二十头牛马的活。我谢谢你。'
       : 'Experienced software engineer with expertise in building startups from 0 to 1. Specializing in React, Next.js, and full-stack development to create innovative, scalable products that drive business growth. View my portfolio and connect for collaboration opportunities.',
   openGraph: {
     title: process.env.NEXT_PUBLIC_DEFAULT_LANGUAGE === 'zh' ? '张浩贤 ｜ 创始人' : 'Iven Zhang | Software Engineer',
     description:
       process.env.NEXT_PUBLIC_DEFAULT_LANGUAGE === 'zh'
-        ? '专修 React.js 和 Next.js, 创业公司全能牛马。前后端通吃, UI 亲自画, 咖啡机坏了也修。我老板曾高度评价: Iven, 你一个人能干二十头牛马的活。我谢谢你。'
+        ? '专修 React.js 和 Next.js, 创业公司全能牛马。前后端通吃, UI 亲自画, 咖啡机坏了也修。我老板曾高度评价我一个人能干二十头牛马的活。我谢谢你。'
         : 'Experienced software engineer with expertise in building startups from 0 to 1. Specializing in React, Next.js, and full-stack development to create innovative, scalable products that drive business growth. View my portfolio and connect for collaboration opportunities.',
     images: [
       {
